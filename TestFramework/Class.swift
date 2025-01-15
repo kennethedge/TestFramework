7de80b8586b89e47c462bb99b733649ba1d715f7
Kenneth <kennethedgetech@gmail.com>
2025-01-15 11:50:33 +0800

    #Initial Commit

---
+++ TestFramework/Class.swift
@@ -0,0 +1,16 @@
+//
+//  Class.swift
+//  TestFramework
+//
+//  Created by KennethLee on 15/01/2025.
+//
+
+import Foundation
+
+public class TestFramework {
+    
+    public static func test() {
+        print("Test")
+    }
+    
+}
